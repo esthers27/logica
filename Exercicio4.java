373592ea6469ce37065c44d4077e6d552f34fb1f
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 17:41:37 +0000

    feat: Exercicio4

--- Exercicio4.java
+++ Exercicio4.java
@@ -7,6 +7,23 @@ public class Exercicio4 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
 		
+		System.out.print("Digite o nome do aluno: ");
+        String nome = input.nextLine();
+
+        System.out.print("Digite a nota da primeira prova: ");
+        double nota1 = input.nextDouble();
+
+        System.out.print("Digite a nota da segunda prova: ");
+        double nota2 = input.nextDouble();
+
+        System.out.print("Digite a nota da terceira prova: ");
+        double nota3 = input.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("Aluno: " + nome);
+        System.out.println("Média: " + media);
+
 		 //escreva seu código aqui
 		
 		input.close();
