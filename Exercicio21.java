edebe43a35c268e0ce095c54a3abee7daf4345cc
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:34:33 +0000

    feat: Exercicio21

--- Exercicio21.java
+++ Exercicio21.java
@@ -6,7 +6,16 @@ import java.util.Scanner;
 public class Exercicio21 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-	
+		int senha;
+
+        System.out.print("Digite a senha: ");
+        senha = input.nextInt();
+
+        if (senha == 1234) {
+            System.out.println("ACESSO PERMITIDO");
+        } else {
+            System.out.println("ACESSO NEGADO");
+        }
 		 //escreva seu código aqui
 		
 		input.close();
