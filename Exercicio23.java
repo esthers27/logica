b997a534fc4c4b428c8ad1495d08b1e99efebaf4
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:37:48 +0000

    feat: Exercicio23

--- Exercicio23.java
+++ Exercicio23.java
@@ -12,6 +12,32 @@ public class Exercicio23 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		int lados;
+        double ladoMedida;
+        double area;
+
+        System.out.print("Digite o número de lados do polígono: ");
+        lados = input.nextInt();
+
+        System.out.print("Digite a medida do lado (cm): ");
+        ladoMedida = input.nextDouble();
+
+        if (lados == 3) {
+            area = (ladoMedida * ladoMedida) * Math.sqrt(3) / 4;
+            System.out.println("TRIÂNGULO");
+            System.out.printf("Área: %.2f cm²%n", area);
+
+        } else if (lados == 4) {
+            area = ladoMedida * ladoMedida;
+            System.out.println("QUADRADO");
+            System.out.printf("Área: %.2f cm²%n", area);
+
+        } else if (lados == 5) {
+            System.out.println("PENTÁGONO");
+
+        } else {
+            System.out.println("Polígono não suportado.");
+        }
 		//escreva seu código aqui
 		
 		input.close();
