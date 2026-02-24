68d56dc279ecafdec93d2b1a718c747ff978f945
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 17:39:05 +0000

    feat: Exercicio3

--- Exercicio3.java
+++ Exercicio3.java
@@ -6,6 +6,12 @@ public class Exercicio3 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Digite um número: ");
+        int numero = input.nextInt();
+
+        if (numero > 20) {
+            System.out.println("Número maior que 20: " + numero);
+        }
 		 //escreva seu código aqui
 
 		input.close();
