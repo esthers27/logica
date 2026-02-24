e73226d53e0a8e8bfbeddafca41225076d598777
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 17:48:54 +0000

    feat: Exercicio7

--- Exercicio7.java
+++ Exercicio7.java
@@ -7,6 +7,18 @@ public class Exercicio7 {
 	public static void main(String[] args) {
 		 Scanner input = new Scanner(System.in); 
 		 
+		double valorDepositado;
+        double rendimento;
+        double valorFinal;
+        
+        System.out.print("Digite o valor depositado: ");
+        valorDepositado = input.nextDouble();
+        
+        rendimento = valorDepositado * 0.007; // 0,70% = 0.007
+        valorFinal = valorDepositado + rendimento;
+        
+        System.out.println("Valor após 1 mês com rendimento: R$ " + valorFinal);
+        
 		 //escreva seu código aqui		 
 
 		input.close();
