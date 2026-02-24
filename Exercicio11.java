0f15281f90d8349211cfbcad06a211e603834db7
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 17:56:30 +0000

    feat: Exercicio11

--- Exercicio11.java
+++ Exercicio11.java
@@ -6,6 +6,36 @@ public class Exercicio11 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
+		int numero;
+
+        System.out.print("Digite um número entre 1 e 7: ");
+        numero = input.nextInt();
+
+        switch (numero) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Número inválido!");
+        }
 		 //escreva seu código aqui
 				
 		input.close();
