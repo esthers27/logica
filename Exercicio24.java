2e8bc581c522fda16c470971fedc63336ebcf896
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:39:50 +0000

    feat: Exercicio24

--- Exercicio24.java
+++ Exercicio24.java
@@ -7,15 +7,26 @@ public class Exercicio24 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		int genero;
-		double altura, pesoIdeal = 0;
-		
-		System.out.print("Digite o sexo (1 - feminino, 2 - masculino): ");
-		genero = input.nextInt();
-		
-		System.out.print("Digite a altura: ");
-		altura = input.nextDouble();
-		
+		 int genero;
+        double altura, pesoIdeal = 0;
+
+        System.out.print("Digite o sexo (1 - feminino, 2 - masculino): ");
+        genero = input.nextInt();
+
+        System.out.print("Digite a altura: ");
+        altura = input.nextDouble();
+
+        if (genero == 1) { // feminino
+            pesoIdeal = (62.1 * altura) - 44.7;
+        } else if (genero == 2) { // masculino
+            pesoIdeal = (72.7 * altura) - 58;
+        } else {
+            System.out.println("Gênero inválido!");
+        }
+
+        if (genero == 1 || genero == 2) {
+            System.out.printf("Peso ideal: %.2f kg%n", pesoIdeal);
+        }
 		 //escreva seu código aqui
 		
 		input.close();
