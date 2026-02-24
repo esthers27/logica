b83a4756bb23e74a2a00b9b607f45a0d14f01153
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 18:27:14 +0000

    feat: Exercicio17

--- Exercicio17.java
+++ Exercicio17.java
@@ -9,15 +9,28 @@ public class Exercicio17 {
 		Scanner input = new Scanner(System.in);
 		
 		int num;
-		double quadrado, cubo, raiz;
-		
-		System.out.print("Digite um número maior do que 0: ");
-		num = input.nextInt();
-		
-		quadrado = Math.pow(num, 2);
-		cubo = Math.pow(num, 3);
-		raiz = Math.sqrt(num);
-		
+        double quadrado, cubo, raiz;
+
+        System.out.print("Digite um número maior que 0: ");
+        num = input.nextInt();
+
+        while (num > 0) {
+
+            quadrado = Math.pow(num, 2);
+            cubo = Math.pow(num, 3);
+            raiz = Math.sqrt(num);
+
+            System.out.println("Número: " + num);
+            System.out.println("Quadrado: " + quadrado);
+            System.out.println("Cubo: " + cubo);
+            System.out.println("Raiz quadrada: " + raiz);
+            System.out.println("-------------------------");
+
+            System.out.print("Digite outro número maior que 0 (ou 0/negativo para sair): ");
+            num = input.nextInt();
+        }
+
+        System.out.println("Programa finalizado.");
 		//escreva seu código aqui
 			
 		input.close();
