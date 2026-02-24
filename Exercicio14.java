e7512e771461baddf08c608791b2daafb62640df
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 18:16:18 +0000

    feat: Exercicio14

--- Exercicio14.java
+++ Exercicio14.java
@@ -9,6 +9,44 @@ import java.util.Scanner;
 public class Exercicio14 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
+		
+		int idade;
+        double peso;
+        int risco;
+
+        System.out.print("Digite a idade: ");
+        idade = input.nextInt();
+
+        System.out.print("Digite o peso: ");
+        peso = input.nextDouble();
+
+        if (idade < 20) {
+            if (peso <= 60) {
+                risco = 9;
+            } else if (peso <= 90) {
+                risco = 8;
+            } else {
+                risco = 7;
+            }
+        } else if (idade <= 50) {
+            if (peso <= 60) {
+                risco = 6;
+            } else if (peso <= 90) {
+                risco = 5;
+            } else {
+                risco = 4;
+            }
+        } else {
+            if (peso <= 60) {
+                risco = 3;
+            } else if (peso <= 90) {
+                risco = 2;
+            } else {
+                risco = 1;
+            }
+        }
+
+        System.out.println("Grupo de risco: " + risco);
 
 		//escreva seu código aqui
 		
