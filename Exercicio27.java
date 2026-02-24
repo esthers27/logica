aacc4bd2b8f690f65b8cdb754b1034c7a68c565f
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:44:54 +0000

    feat: Exercicio27

--- Exercicio27.java
+++ Exercicio27.java
@@ -8,6 +8,26 @@ import java.util.Scanner;
 public class Exercicio27 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		double lado1, lado2, lado3;
+
+        System.out.print("Digite o primeiro lado: ");
+        lado1 = input.nextDouble();
+
+        System.out.print("Digite o segundo lado: ");
+        lado2 = input.nextDouble();
+
+        System.out.print("Digite o terceiro lado: ");
+        lado3 = input.nextDouble();
+
+        if (lado1 == lado2 && lado2 == lado3) {
+            System.out.println("Triângulo Equilátero");
+        } 
+        else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            System.out.println("Triângulo Isósceles");
+        } 
+        else {
+            System.out.println("Triângulo Escaleno");
+        }
 
 		 //escreva seu código aqui
 		
