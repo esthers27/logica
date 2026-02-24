5b77eb3c04497a5a841d98e75d7b46c5582f0a6e
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:46:19 +0000

    feat: Exercicio28

--- Exercicio28.java
+++ Exercicio28.java
@@ -8,7 +8,26 @@ import java.util.Scanner;
 public class Exercicio28 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+		double a, b, c;
+
+        System.out.print("Digite o primeiro ângulo: ");
+        a = input.nextDouble();
+
+        System.out.print("Digite o segundo ângulo: ");
+        b = input.nextDouble();
+
+        System.out.print("Digite o terceiro ângulo: ");
+        c = input.nextDouble();
+
+        if (a == 90 || b == 90 || c == 90) {
+            System.out.println("Triângulo Retângulo");
+        } 
+        else if (a > 90 || b > 90 || c > 90) {
+            System.out.println("Triângulo Obtusângulo");
+        } 
+        else {
+            System.out.println("Triângulo Acutângulo");
+        }
 		 //escreva seu código aqui
 		
 		input.close();
