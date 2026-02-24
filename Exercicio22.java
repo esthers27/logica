b23ffbc4170b0b9d2f8f22119f4caf82274e56de
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:36:24 +0000

    feat: Exercicio22

--- Exercicio22.java
+++ Exercicio22.java
@@ -7,15 +7,49 @@ public class Exercicio22 {
 		Scanner input = new Scanner(System.in);
 		
 		int num1, num2, num3;
-		
-		System.out.print("Digite o primeiro valor: ");
-		num1 = input.nextInt();
-		
-		System.out.print("Digite o segundo valor: ");
-		num2 = input.nextInt();
-		
-		System.out.print("Digite o terceiro valor: ");
-		num3 = input.nextInt();
+
+        System.out.print("Digite o primeiro valor: ");
+        num1 = input.nextInt();
+
+        System.out.print("Digite o segundo valor: ");
+        num2 = input.nextInt();
+
+        System.out.print("Digite o terceiro valor: ");
+        num3 = input.nextInt();
+
+        int menor, meio, maior;
+
+        // Encontrar o menor
+        if (num1 < num2 && num1 < num3) {
+            menor = num1;
+            if (num2 < num3) {
+                meio = num2;
+                maior = num3;
+            } else {
+                meio = num3;
+                maior = num2;
+            }
+        } else if (num2 < num1 && num2 < num3) {
+            menor = num2;
+            if (num1 < num3) {
+                meio = num1;
+                maior = num3;
+            } else {
+                meio = num3;
+                maior = num1;
+            }
+        } else {
+            menor = num3;
+            if (num1 < num2) {
+                meio = num1;
+                maior = num2;
+            } else {
+                meio = num2;
+                maior = num1;
+            }
+        }
+
+        System.out.println("Ordem crescente: " + menor + " " + meio + " " + maior);
 		
 		 //escreva seu código aqui
 		
