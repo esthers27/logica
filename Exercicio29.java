9819172ead21c5592af5050765d660714c9d9dfa
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:48:09 +0000

    feat: Exercicio29

--- Exercicio29.java
+++ Exercicio29.java
@@ -2,10 +2,20 @@
 
 import java.util.Scanner;
 
-public class Exerc29 {
+public class Exercicio29 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
+		int numero;
+
+        System.out.print("Digite um número: ");
+        numero = input.nextInt();
+
+        if (numero % 3 == 0) {
+            System.out.println("É múltiplo de 3");
+        } else {
+            System.out.println("Não é múltiplo de 3");
+        }
 		//escreva seu código aqui
 		
 		input.close();
