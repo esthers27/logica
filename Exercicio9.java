c7422363a70fa42ead086273ed4a39caf58041b1
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 17:52:39 +0000

    feat: Exercicio9

--- Exercicio9.java
+++ Exercicio9.java
@@ -7,6 +7,25 @@ import java.util.Scanner;
 public class Exercicio9 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
+		
+        int idade;
+
+        System.out.print("Digite a idade do nadador: ");
+        idade = input.nextInt();
+
+        if (idade < 5) {
+            System.out.println("Categoria: nenhuma");
+        } else if (idade <= 7) {
+            System.out.println("Categoria: infantil");
+        } else if (idade <= 10) {
+            System.out.println("Categoria: juvenil");
+        } else if (idade <= 15) {
+            System.out.println("Categoria: adolescente");
+        } else if (idade <= 30) {
+            System.out.println("Categoria: adulto");
+        } else {
+            System.out.println("Categoria: sênior");
+        }
 
 		 //escreva seu código aqui
 
