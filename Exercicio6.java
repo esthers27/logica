711366701afb810c339b3981d6a7a56b4565de38
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 17:47:30 +0000

    feat: Exercicio6

--- Exercicio6.java
+++ Exercicio6.java
@@ -7,6 +7,20 @@ public class Exercicio6 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		double cotacaoDolar;
+        double quantidadeDolar;
+        double valorEmReais;
+        
+        System.out.print("Digite a cotação do dólar: ");
+        cotacaoDolar = input.nextDouble();
+        
+        System.out.print("Digite a quantidade de dólares: ");
+        quantidadeDolar = input.nextDouble();
+        
+        valorEmReais = cotacaoDolar * quantidadeDolar;
+        
+        System.out.println("Valor em reais (R$): " + valorEmReais);
+
 		 //escreva seu código aqui
 
 		 input.close();
