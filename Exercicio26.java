d896f3239cde46b46a6c23e4fbb44899f58b618b
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:43:45 +0000

    feat: Exercicio26

--- Exercicio26.java
+++ Exercicio26.java
@@ -7,6 +7,27 @@ public class Exercicio26 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		int num1, num2, num3;
+        int maior;
+
+        System.out.print("Digite o primeiro valor: ");
+        num1 = input.nextInt();
+
+        System.out.print("Digite o segundo valor: ");
+        num2 = input.nextInt();
+
+        System.out.print("Digite o terceiro valor: ");
+        num3 = input.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+            maior = num1;
+        } else if (num2 > num1 && num2 > num3) {
+            maior = num2;
+        } else {
+            maior = num3;
+        }
+
+        System.out.println("O maior valor é: " + maior);
 		 //escreva seu código aqui
 		
 		input.close();
