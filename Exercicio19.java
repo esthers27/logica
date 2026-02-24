31b06509501e9e0ee0c30f45a683c183109244dd
Esther Souza <135926875+esthers27@users.noreply.github.com>
2026-02-24 19:31:35 +0000

    feat: Exercicio19

--- Exercicio19.java
+++ Exercicio19.java
@@ -5,9 +5,19 @@ import java.util.Scanner;
 public class Exercicio19 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+ 		short a, b;
 
-		short a, b;
-		
+        System.out.print("Digite o primeiro valor: ");
+        a = input.nextShort();
+
+        System.out.print("Digite o segundo valor: ");
+        b = input.nextShort();
+
+        if (a > b) {
+            System.out.println("O maior valor é: " + a);
+        } else {
+            System.out.println("O maior valor é: " + b);
+        }
 		 //escreva seu código aqui
 		
 		input.close();
